c17fdb3f49196c7da64641e73d9e9086ca294d0a
ileolami <makindemercymiracle@gmail.com>
2023-11-24 11:54:07 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Route,
-  Link,
-} from "react-router-dom";
+import { BrowserRouter,Routes, Route } from "react-router-dom"
+
 import HomePage from './components/PatientsRegister';
 import SignIn from './components/PatientsSign';
 
 const App = () => {
   return (
-    <HomePage/>
-    
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/homepage" element={<HomePage />} />
+        <Route path="/signin" element={<SignIn />} />
+      </Routes>
+    </BrowserRouter>
   );
-  
 };
 
 export default App;
--- src/components/PatientsRegister.jsx
+++ src/components/PatientsRegister.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import Web3 from 'web3';
 import '../index.css'
 import { ConnectBtn, SignUpButton } from './Button'
 import ContractJson from '../Healthcare.json'
 import SignIn from './PatientsSign';
 
-function HomePage() {
+function PRegister() {
+
+
+
   return (
 <main className="flex justify-center items-center">
   <form action="" className=" bg-purple-background-form text-border-form  m-4 mr-10 p-10 h-2/6 rounded-3xl shadow-2xl shadow-purple-button-first sm:max sm:m-36">
@@ -46,7 +49,10 @@ function HomePage() {
     </div>
     <div className='flex justify-center items-center'><SignUpButton buttonText="Sign Up" /></div>
     <div>
-      <p className='text-center text-xs font-extralight'>Already have an account?<a href=""><SignIn/></a></p>
+      <p className='text-center text-xs font-extralight'>Already have an account?<Link to={'/signin'}> Click Me</Link></p>
+    {/* <span onClick={()=>{
+      navigate('/signin')
+    }}>Click Me</span> */}
     </div>
   </form>
 </main>
@@ -54,4 +60,4 @@ function HomePage() {
   )
 }
 
-export default HomePage
+export default PRegister
